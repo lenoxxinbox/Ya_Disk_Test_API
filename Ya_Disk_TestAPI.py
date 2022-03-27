77179c4b3670af25b46cba8df215707ac19a0bff
lenoxxinbox <68026634+lenoxxinbox@users.noreply.github.com>
2022-03-27 17:04:46 +0300

    Redact_10

--- Ya_Disk_TestAPI.py
+++ Ya_Disk_TestAPI.py
@@ -9,6 +9,7 @@ headers = {'Content-Type': 'application/json', 'Authorization': 'OAuth {}'.forma
 params = {"path": 'TestFolder'}
 params_f = {'path': 'TestFolder/Test_image',
                         'url': 'https://kartinkin.net/uploads/posts/2021-03/1616125516_57-p-elbrus-krasivie-foto-58.jpg'}
+params_c = {'from': 'TestFolder/Test_image', 'path': 'Test_image_00'}
 params_l = {'limit': 5}
 
 
@@ -21,6 +22,7 @@ class YaDiskFolders:
 
     def test_append_folder(self):
         """Test_01. Создание папки на YandexDisk. Метод Put"""
+        print()
         response = requests.put(url_API, headers=headers, params=params)
         print('Test_01. Creating a Folder on YandexDisk. Put method')
         print(f'Status Code = {response.status_code}')
@@ -42,6 +44,7 @@ class YaDiskFolders:
 
     def test_download_file(self):
         """Test_02. Загрузка файла в папку на YandexDisk. Метод Post"""
+        print()
         download_file = requests.post(url=url_API + '/upload', headers=headers, params=params_f)
         print('Test_02. Uploading a file to a Folder on YandexDisk. Post Method')
         print(f'Status Code = {download_file.status_code}')
@@ -61,10 +64,33 @@ class YaDiskFolders:
             with open('report.txt', 'a') as rep:
                 rep.write(report)
 
+    def test_copy_file(self):
+        """Test_03. Копирование файла в папку на YandexDisk. Метод Post"""
+        print()
+        copy_file = requests.post(url=url_API + '/copy', headers=headers, params=params_c)
+        print('Test_03. Copying a file to a Folder on YandexDisk. Post Method')
+        print(f'Status Code = {copy_file.status_code}')
+        if copy_file.status_code == 201:
+            print(f'{params_c["path"]} copying successful. Test is pass')
+            report = f'{datetime.now()} TEST IS PASS! The file {params_c["path"]} is copying on the YaDisk\n' \
+                     f'Status_code = {copy_file.status_code}\n'
+            with open('report.txt', 'a') as rep:
+                rep.write(report)
+        else:
+            print(f'Downloading file {params_f["path"]} is fail')
+            print(copy_file.text)
+            print(copy_file.headers)
+            report = f'{datetime.now()} TEST IS FAILED! The file {params_c["path"]} ' \
+                     f'IS NOT copying on the YaDisk\n' \
+                     f'Status_code = {copy_file.status_code}\n'
+            with open('report.txt', 'a') as rep:
+                rep.write(report)
+
     def test_file_list(self):
-        """Тест_03. Получение списка файлов YandexDisk. Метод Get"""
+        """Тест_04. Получение списка файлов YandexDisk. Метод Get"""
+        print()
         resp = requests.get(url=url_API + '/files', headers=headers, params=params_l)
-        print('Test_03. Getting a list of YandexDisk files. Get Method')
+        print('Test_04. Getting a list of YandexDisk files. Get Method')
         print(f'Status Code = {resp.status_code}')
         if resp.status_code == 200:
             print('List generated. Test is pass')
@@ -88,9 +114,10 @@ class YaDiskFolders:
             # exit(2)
 
     def test_delete_folder(self):
-        """Test_04. Удаление папки c YandexDisk. Метод Delete"""
+        """Test_05. Удаление папки c YandexDisk. Метод Delete"""
+        print()
         response = requests.delete(url_API, headers=headers, params=params)
-        print('Test_04. Deleting the folder with YandexDisk. Delete method')
+        print('Test_05. Deleting the folder with YandexDisk. Delete method')
         print(f'Status Code = {response.status_code}')
         if response.status_code == 200 or 202 or 204:
             print(f'{params["path"]} deleted successful. Test is pass')
@@ -113,9 +140,13 @@ def main():
     test_api = YaDiskFolders(url_API, headers, params)
 
     test_api.test_append_folder()
+    test_api.test_file_list()
     test_api.test_download_file()
     test_api.test_file_list()
+    test_api.test_copy_file()
+    test_api.test_file_list()
     test_api.test_delete_folder()
+    test_api.test_file_list()
 
 
 if __name__ == '__main__':
